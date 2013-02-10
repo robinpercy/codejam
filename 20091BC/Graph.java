ce00331ce554179bd0cdc59b9a419029d42ce71a
Robin Percy <rpercy@gmail.com>
2013-02-09 16:04:22 -0800

    Added simple graph datastructure

---
+++ 20091BC/Graph.java
@@ -0,0 +1,76 @@
+
+import static java.lang.System.out;
+import java.util.*;
+
+public class Graph {
+    EdgeNode[] edges;
+    int[] degree;
+    int nvertices;
+    int nedges;
+    boolean directed;
+
+    public Graph(int maxEdges, boolean directed) {
+
+        nvertices = 0;
+        nedges = 0;
+        directed = directed;
+
+        edges = new EdgeNode[maxEdges];
+        degree = new int[maxEdges];
+    }
+
+    public static class EdgeNode {
+        int y;
+        int weight;
+        EdgeNode next;
+    }
+
+    public void insertEdge(int x, int y, boolean directed) {
+        EdgeNode p = new EdgeNode();
+        p.y = y;
+        p.next = edges[x];
+        edges[x] = p;
+        degree[x]++;
+        // If directed, add the other edge and only increment the edge count once we're done
+        if (!directed)
+            insertEdge(y, x, true);
+        else
+            nedges++;
+    }
+
+    public static Graph readGraph(boolean directed) {
+        int i;
+        int m;
+        int x, y;
+
+        Graph g = new Graph(1000, directed);
+        Scanner scanner = new Scanner(System.in);
+        g.nvertices = scanner.nextInt();
+        m = scanner.nextInt();
+
+        for (i=0; i<m; i++) {
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            g.insertEdge(x, y, directed);
+        }
+        return g;
+    }
+
+    public void printGraph() {
+        for(int i=0; i < nvertices; i++) {
+            out.format("%d:", i);
+            EdgeNode p = edges[i];
+            while (p != null) {
+                out.format(" %d", p.y);
+                p = p.next;
+            }
+            out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph g = readGraph(false);
+        g.printGraph();
+    }
+}
+
